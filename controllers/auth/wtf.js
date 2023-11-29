3e817877c86194987198f52c6ed075810a897a48
argojkee <maksymhaidabura@gmail.com>
2023-11-29 20:17:52 +0200

    async await

---
+++ controllers/auth/wtf.js
@@ -0,0 +1,14 @@
+// const login = require("./login");
+
+// describe("login", () => {
+//   test("response contains token", async () => {
+//     const data = await login({
+//       body: { email: "maksym@gmail.com", password: "123456" },
+//     });
+//     expect(data).toContain("token");
+//   });
+// });
+
+// login({ body: { email: "maksym@gmail.com", password: "123456" } }).then(
+//   (resp) => console.log("ololo")
+// );
